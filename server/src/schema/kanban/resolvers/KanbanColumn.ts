c47c904fa8b0a84b282ba49969f2f68a7da93a1b
Vineet Sridhar <vineets1600@gmail.com>
2026-03-02 16:24:30 -0500

    Fix render deploy issues

--- server/src/schema/kanban/resolvers/KanbanColumn.ts
+++ server/src/schema/kanban/resolvers/KanbanColumn.ts
@@ -2,6 +2,6 @@ import type { KanbanColumnResolvers } from './../../types.generated';
 export const KanbanColumn: KanbanColumnResolvers = {
     tickets: ({ tickets }, _arg, _ctx) => {
                         /* KanbanColumn.tickets resolver is required because KanbanColumn.tickets and KanbanColumnMapper.tickets are not compatible */
-                        return tickets
+                        return tickets ?? []
                       }
 };
--- server/src/schema/kanban/resolvers/TicketAttachment.ts
+++ server/src/schema/kanban/resolvers/TicketAttachment.ts
@@ -2,6 +2,6 @@ import type { TicketAttachmentResolvers } from './../../types.generated';
 export const TicketAttachment: TicketAttachmentResolvers = {
     url: ({ url }, _arg, _ctx) => {
                         /* TicketAttachment.url resolver is required because TicketAttachment.url and TicketAttachmentMapper.url are not compatible */
-                        return url
+                        return url ?? ''
                       }
 };
